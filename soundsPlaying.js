37775249a7dc052eaaa1e08806d5792131e5e8e2
Stephi <139639918+Stephi-x3@users.noreply.github.com>
2023-09-06 12:12:48 +0200

    Update soundsPlaying.js

--- soundsPlaying.js
+++ soundsPlaying.js
@@ -28,19 +28,4 @@ keys.forEach(key => key.addEventListener('touchstart', playSound, false));
 window.addEventListener('keydown', playSound);
 
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-//transitionend: when a CSS transition has completed
-/*
-// Eine Funktion, die auf das Touch-Ereignis reagiert
-function meineFunktion() {
-    const p = document.createElement("p");
-    p.textContent = "Element berührt!";
-    document.getElementById("output").appendChild(p);
-}
-
-// Das HTML-Element auswählen, auf das wir reagieren möchten == keys
-var touchClick = document.getElementById('touchEvent');
-
-
-// Ein Touch-Ereignis hinzufügen und die Funktion verknüpfen
-touchClick.addEventListener('touchstart', meineFunktion, false);*/
 
