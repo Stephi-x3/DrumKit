ec29a461c0a9d04e9a43aa4928842a613ae4bacb
Stephi <139639918+Stephi-x3@users.noreply.github.com>
2023-09-08 11:11:43 +0200

    test touch and keyboard support

---
+++ checkKeyboardAvailability.js
@@ -0,0 +1,13 @@
+// Check touchSupport and keyboardSupport
+var touchSupport = 'ontouchstart' in window || navigator.maxTouchPoints || 'TouchEvent' in window;
+var keyboardSupport = navigator.keyboard || 'KeyboardEvent' in window;
+
+var addHowToDescription = document.getElementById('firstContainer');
+
+    if (touchSupport && keyboardSupport) {
+        addHowToDescription.textContent = 'Play sounds by hitting the keys on the keyboard OR simply touch the keys!';
+    } else if (!keyboardSupport) {
+        addHowToDescription.textContent = 'Touch the keys to play sounds!';
+    } else {
+        addHowToDescription.textContent = 'Play sounds by hitting the keys!';
+    }
