5f992d58db7fdf2c008511a8ed720d2d9b5cacd6
Stephi <139639918+Stephi-x3@users.noreply.github.com>
2023-09-08 11:27:13 +0200

    Update checkKeyboardAvailability.js

--- checkKeyboardAvailability.js
+++ checkKeyboardAvailability.js
@@ -1,13 +1,14 @@
 // Check touchSupport and keyboardSupport
+// It is assumed that a smartphone is being used when the width is < 400
 var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-var touchSupport = 'ontouchstart' in window || navigator.maxTouchPoints || (windowWidth<400);
+var touchSupport = 'ontouchstart' in window || navigator.maxTouchPoints;
 var keyboardSupport = navigator.keyboard || 'KeyboardEvent' in window;
 
 var addHowToDescription = document.getElementById('firstContainer');
 
     if (touchSupport && keyboardSupport) {
         addHowToDescription.textContent = 'Play sounds by hitting the keys on the keyboard OR simply touch the keys!';
-    } else if (!keyboardSupport) {
+    } else if (!keyboardSupport || (windowWidth<400)) {
         addHowToDescription.textContent = 'Touch the keys to play sounds!';
     } else {
         addHowToDescription.textContent = 'Play sounds by hitting the keys!';
