b433fdaf6b980e0cf9b93f91e03d55bbfb8d5bf9
Stephi <139639918+Stephi-x3@users.noreply.github.com>
2023-09-08 11:55:15 +0200

    Update checkKeyboardAvailability.js

--- checkKeyboardAvailability.js
+++ checkKeyboardAvailability.js
@@ -2,29 +2,14 @@
 // It is assumed that a smartphone is being used when the width is < 400
 var windowWidth = window.screen.width;
 var touchSupport = 'ontouchstart' in window || navigator.maxTouchPoints;
-var keyboardSupport = navigator.keyboard;
+var keyboardSupport = navigator.keyboard || 'KeyboardEvent' in window;
 
 var addHowToDescription = document.getElementById('firstContainer');
 
-    if (touchSupport && keyboardSupport) {
+    if (touchSupport && keyboardSupport && !(windowWidth<400)) {
         addHowToDescription.textContent = 'Play sounds by hitting the keys on the keyboard OR simply touch the keys!';
-    } else if (!keyboardSupport) {
+    } else if (!keyboardSupport || (windowWidth<400)) {
         addHowToDescription.textContent = 'Touch the keys to play sounds!';
     } else {
         addHowToDescription.textContent = 'Play sounds by hitting the keys!';
     }
-
-/*
-    var windowWidth = window.screen.width;
-    var touchSupport = 'ontouchstart' in window || navigator.maxTouchPoints;
-    var keyboardSupport = navigator.keyboard || 'KeyboardEvent' in window;
-    
-    var addHowToDescription = document.getElementById('firstContainer');
-    
-        if (touchSupport && keyboardSupport && !(windowWidth<400)) {
-            addHowToDescription.textContent = 'Play sounds by hitting the keys on the keyboard OR simply touch the keys!';
-        } else if (!keyboardSupport || (windowWidth<400)) {
-            addHowToDescription.textContent = 'Touch the keys to play sounds!';
-        } else {
-            addHowToDescription.textContent = 'Play sounds by hitting the keys!';
-        }*/
