ff1ba44625a965da80bbec0e2663c509086c790e
Stephi <139639918+Stephi-x3@users.noreply.github.com>
2023-09-08 10:24:00 +0200

    comments

--- heightOfElement.js
+++ heightOfElement.js
@@ -1,4 +1,6 @@
+//get the heigt of the container
 var heightOfContainer = document.getElementById('firstContainer').offsetHeight;
 
+//get the container where the heigt will be dynamically calculated
 var containerGetsNewHeight = document.getElementsByClassName('keys')[0];
 containerGetsNewHeight.style.height = `calc(100vh - ${heightOfContainer}px)`;
